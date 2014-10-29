d141678935cc55ec94aa57c1fe886e34bf701d95
Joshua Rubin <joshuarubin@users.noreply.github.com>
2014-10-29 00:23:58 -0600

    Update java.plugin.zsh

--- java.plugin.zsh
+++ java.plugin.zsh
@@ -3,4 +3,5 @@ if [[ -r $java_home ]]; then
   export JAVA_HOME=`$java_home 2> /dev/null`
 elif [[ -d "$HOME/working/jdk/" ]]; then
   export JAVA_HOME="$HOME/working/jdk"
+  PATH="$HOME/working/jdk/bin:$PATH"
 fi
