a5a9668aecfea01a9778660f200e12c802ca37ce
Joshua Rubin <joshuarubin@users.noreply.github.com>
2014-10-29 00:21:29 -0600

    Create java.plugin.zsh

---
+++ java.plugin.zsh
@@ -0,0 +1,6 @@
+local java_home="/usr/libexec/java_home"
+if [[ -r $java_home ]]; then
+  export JAVA_HOME=`$java_home 2> /dev/null`
+elif [[ -d "$HOME/working/jdk/" ]]; then
+  export JAVA_HOME="$HOME/working/jdk"
+fi
