860a9ade0f9ceb670196693bb142393984a37c51
Joshua Rubin <jrubin@zvelo.com>
2014-10-29 01:05:37 -0600

    update
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- java.plugin.zsh
+++ java.plugin.zsh
@@ -1,7 +1,7 @@
 local java_home="/usr/libexec/java_home"
 if [[ -d "$HOME/working/jdk/" ]]; then
   export JAVA_HOME="$HOME/working/jdk"
-  PATH="$HOME/working/jdk/bin:$PATH"
+  path[1,0]="$HOME/working/jdk/bin"
 elif [[ -r $java_home ]]; then
   export JAVA_HOME=`$java_home 2> /dev/null`
 fi
